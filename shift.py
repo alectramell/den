bcfb0f78e6e4a96f89124a9e555ad8b700922d76
alectramell <alectramell@gmail.com>
2021-07-17 19:22:45 -0600

    den

--- shift.py
+++ shift.py
@@ -1,50 +1,41 @@
 import os
 import sys
-import platform
+import time
+import string
+from threading import Thread
 
-OSname = str(platform.system())
+def shift(xvar, yvar):
 
-if OSname == "Windows":
+	xnum = int(xvar)
+	xstr = str(yvar.lower())
 
-	def clear():
+	ALPHA = list(string.ascii_lowercase[0:26])
+	SHIFT = list(string.ascii_lowercase[xnum:26]+string.ascii_lowercase[0:xnum])
+	
+	xmid = xstr.replace(ALPHA[0], '.00').replace(ALPHA[1], '.01').replace(ALPHA[2], '.02').replace(ALPHA[3], '.03').replace(ALPHA[4], '.04').replace(ALPHA[5], '.05').replace(ALPHA[6], '.06').replace(ALPHA[7], '.07').replace(ALPHA[8], '.08').replace(ALPHA[9], '.09').replace(ALPHA[10], '.10').replace(ALPHA[11], '.11').replace(ALPHA[12], '.12').replace(ALPHA[13], '.13').replace(ALPHA[14], '.14').replace(ALPHA[15], '.15').replace(ALPHA[16], '.16').replace(ALPHA[17], '.17').replace(ALPHA[18], '.18').replace(ALPHA[19], '.19').replace(ALPHA[20], '.20').replace(ALPHA[21], '.21').replace(ALPHA[22], '.22').replace(ALPHA[23], '.23').replace(ALPHA[24], '.24').replace(ALPHA[25], '.25')
+	xout = xmid.replace('.00', SHIFT[0]).replace('.01', SHIFT[1]).replace('.02', SHIFT[2]).replace('.03', SHIFT[3]).replace('.04', SHIFT[4]).replace('.05', SHIFT[5]).replace('.06', SHIFT[6]).replace('.07', SHIFT[7]).replace('.08', SHIFT[8]).replace('.09', SHIFT[9]).replace('.10', SHIFT[10]).replace('.11', SHIFT[11]).replace('.12', SHIFT[12]).replace('.13', SHIFT[13]).replace('.14', SHIFT[14]).replace('.15', SHIFT[15]).replace('.16', SHIFT[16]).replace('.17', SHIFT[17]).replace('.18', SHIFT[18]).replace('.19', SHIFT[19]).replace('.20', SHIFT[20]).replace('.21', SHIFT[21]).replace('.22', SHIFT[22]).replace('.23', SHIFT[23]).replace('.24', SHIFT[24]).replace('.25', SHIFT[25])
 
-		import os
-		os.system('cls')
-else:
+	return(xout)
 
-	def clear():
+def unshift(xvar, yvar):
 
-		import os
-		os.system('clear')
+	xnum = int(xvar)
+	xstr = str(yvar.lower())
 
-def shift(XVAR):
+	ALPHA = list(string.ascii_lowercase[0:26])
+	SHIFT = list(string.ascii_lowercase[xnum:26]+string.ascii_lowercase[0:xnum])
+	
+	xmid = xstr.replace(SHIFT[0], '.00').replace(SHIFT[1], '.01').replace(SHIFT[2], '.02').replace(SHIFT[3], '.03').replace(SHIFT[4], '.04').replace(SHIFT[5], '.05').replace(SHIFT[6], '.06').replace(SHIFT[7], '.07').replace(SHIFT[8], '.08').replace(SHIFT[9], '.09').replace(SHIFT[10], '.10').replace(SHIFT[11], '.11').replace(SHIFT[12], '.12').replace(SHIFT[13], '.13').replace(SHIFT[14], '.14').replace(SHIFT[15], '.15').replace(SHIFT[16], '.16').replace(SHIFT[17], '.17').replace(SHIFT[18], '.18').replace(SHIFT[19], '.19').replace(SHIFT[20], '.20').replace(SHIFT[21], '.21').replace(SHIFT[22], '.22').replace(SHIFT[23], '.23').replace(SHIFT[24], '.24').replace(SHIFT[25], '.25')
+	xout = xmid.replace('.00', ALPHA[0]).replace('.01', ALPHA[1]).replace('.02', ALPHA[2]).replace('.03', ALPHA[3]).replace('.04', ALPHA[4]).replace('.05', ALPHA[5]).replace('.06', ALPHA[6]).replace('.07', ALPHA[7]).replace('.08', ALPHA[8]).replace('.09', ALPHA[9]).replace('.10', ALPHA[10]).replace('.11', ALPHA[11]).replace('.12', ALPHA[12]).replace('.13', ALPHA[13]).replace('.14', ALPHA[14]).replace('.15', ALPHA[15]).replace('.16', ALPHA[16]).replace('.17', ALPHA[17]).replace('.18', ALPHA[18]).replace('.19', ALPHA[19]).replace('.20', ALPHA[20]).replace('.21', ALPHA[21]).replace('.22', ALPHA[22]).replace('.23', ALPHA[23]).replace('.24', ALPHA[24]).replace('.25', ALPHA[25])
 
-	word = str(XVAR.lower())
+	return(xout)
 
-	XMARKS = list(['.00','.01','.02','.03','.04','.05','.06','.07','.08','.09','.10','.11','.12','.13','.14','.15','.16','.17','.18','.19','.20','.21','.22','.23','.24','.25'])
-
-	XALPHA = list(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-
-	XSHIFT = list(['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'])
-
-	XTRANS = str(word.replace(XALPHA[0], XMARKS[0]).replace(XALPHA[1], XMARKS[1]).replace(XALPHA[2], XMARKS[2]).replace(XALPHA[3], XMARKS[3]).replace(XALPHA[4], XMARKS[4]).replace(XALPHA[5], XMARKS[5]).replace(XALPHA[6], XMARKS[6]).replace(XALPHA[7], XMARKS[7]).replace(XALPHA[8], XMARKS[8]).replace(XALPHA[9], XMARKS[9]).replace(XALPHA[10], XMARKS[10]).replace(XALPHA[11], XMARKS[11]).replace(XALPHA[12], XMARKS[12]).replace(XALPHA[13], XMARKS[13]).replace(XALPHA[14], XMARKS[14]).replace(XALPHA[15], XMARKS[15]).replace(XALPHA[16], XMARKS[16]).replace(XALPHA[17], XMARKS[17]).replace(XALPHA[18], XMARKS[18]).replace(XALPHA[19], XMARKS[19]).replace(XALPHA[20], XMARKS[20]).replace(XALPHA[21], XMARKS[21]).replace(XALPHA[22], XMARKS[22]).replace(XALPHA[23], XMARKS[23]).replace(XALPHA[24], XMARKS[24]).replace(XALPHA[25], XMARKS[25]))
-
-	XOUT = str(XTRANS.replace(XMARKS[0], XSHIFT[0]).replace(XMARKS[1], XSHIFT[1]).replace(XMARKS[2], XSHIFT[2]).replace(XMARKS[3], XSHIFT[3]).replace(XMARKS[4], XSHIFT[4]).replace(XMARKS[5], XSHIFT[5]).replace(XMARKS[6], XSHIFT[6]).replace(XMARKS[7], XSHIFT[7]).replace(XMARKS[8], XSHIFT[8]).replace(XMARKS[9], XSHIFT[9]).replace(XMARKS[10], XSHIFT[10]).replace(XMARKS[11], XSHIFT[11]).replace(XMARKS[12], XSHIFT[12]).replace(XMARKS[13], XSHIFT[13]).replace(XMARKS[14], XSHIFT[14]).replace(XMARKS[15], XSHIFT[15]).replace(XMARKS[16], XSHIFT[16]).replace(XMARKS[17], XSHIFT[17]).replace(XMARKS[18], XSHIFT[18]).replace(XMARKS[19], XSHIFT[19]).replace(XMARKS[20], XSHIFT[20]).replace(XMARKS[21], XSHIFT[21]).replace(XMARKS[22], XSHIFT[22]).replace(XMARKS[23], XSHIFT[23]).replace(XMARKS[24], XSHIFT[24]).replace(XMARKS[25], XSHIFT[25]))
-
-	return(XOUT)
-
-def unshift(XVAR):
-
-	word = str(XVAR)
-
-	XMARKS = list(['.00','.01','.02','.03','.04','.05','.06','.07','.08','.09','.10','.11','.12','.13','.14','.15','.16','.17','.18','.19','.20','.21','.22','.23','.24','.25'])
-
-	XALPHA = list(['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'])
-
-	XSHIFT = list(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-
-	XTRANS = str(word.replace(XALPHA[0], XMARKS[0]).replace(XALPHA[1], XMARKS[1]).replace(XALPHA[2], XMARKS[2]).replace(XALPHA[3], XMARKS[3]).replace(XALPHA[4], XMARKS[4]).replace(XALPHA[5], XMARKS[5]).replace(XALPHA[6], XMARKS[6]).replace(XALPHA[7], XMARKS[7]).replace(XALPHA[8], XMARKS[8]).replace(XALPHA[9], XMARKS[9]).replace(XALPHA[10], XMARKS[10]).replace(XALPHA[11], XMARKS[11]).replace(XALPHA[12], XMARKS[12]).replace(XALPHA[13], XMARKS[13]).replace(XALPHA[14], XMARKS[14]).replace(XALPHA[15], XMARKS[15]).replace(XALPHA[16], XMARKS[16]).replace(XALPHA[17], XMARKS[17]).replace(XALPHA[18], XMARKS[18]).replace(XALPHA[19], XMARKS[19]).replace(XALPHA[20], XMARKS[20]).replace(XALPHA[21], XMARKS[21]).replace(XALPHA[22], XMARKS[22]).replace(XALPHA[23], XMARKS[23]).replace(XALPHA[24], XMARKS[24]).replace(XALPHA[25], XMARKS[25]))
-
-	XOUT = str(XTRANS.replace(XMARKS[0], XSHIFT[0]).replace(XMARKS[1], XSHIFT[1]).replace(XMARKS[2], XSHIFT[2]).replace(XMARKS[3], XSHIFT[3]).replace(XMARKS[4], XSHIFT[4]).replace(XMARKS[5], XSHIFT[5]).replace(XMARKS[6], XSHIFT[6]).replace(XMARKS[7], XSHIFT[7]).replace(XMARKS[8], XSHIFT[8]).replace(XMARKS[9], XSHIFT[9]).replace(XMARKS[10], XSHIFT[10]).replace(XMARKS[11], XSHIFT[11]).replace(XMARKS[12], XSHIFT[12]).replace(XMARKS[13], XSHIFT[13]).replace(XMARKS[14], XSHIFT[14]).replace(XMARKS[15], XSHIFT[15]).replace(XMARKS[16], XSHIFT[16]).replace(XMARKS[17], XSHIFT[17]).replace(XMARKS[18], XSHIFT[18]).replace(XMARKS[19], XSHIFT[19]).replace(XMARKS[20], XSHIFT[20]).replace(XMARKS[21], XSHIFT[21]).replace(XMARKS[22], XSHIFT[22]).replace(XMARKS[23], XSHIFT[23]).replace(XMARKS[24], XSHIFT[24]).replace(XMARKS[25], XSHIFT[25]))
-
-	return(XOUT.lower())
+# Usage..
+#
+#	>>> from shift import *
+#	>>> 
+#	>>> shift(5, 'hello world')
+#	>>> 'mjqqt btwqi'
+#	>>>
+#	>>> unshift(5, 'mjqqt btwqi')
+#	>>> 'hello world'
